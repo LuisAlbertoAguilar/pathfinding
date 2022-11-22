ee54cf4a7f549af0deb8f299f5c8b347d27c2451
LuisAlbertoAguilar <luisalberto.aguilar.gonzalez1@gmail.com>
2022-11-22 12:40:47 -0600

    initial commit

---
+++ lib/pathfinding.ex
@@ -0,0 +1,16 @@
+defmodule Pathfinding do
+  def ex1 do
+    [
+      ~w[X X X X X X X X X X],
+      ~w[X _ _ _ X X _ X _ X],
+      ~w[X _ X _ _ X _ _ _ X],
+      ~w[X _ X X _ _ _ X _ X],
+      ~w[X _ X _ _ X _ _ _ X],
+      ~w[X _ _ _ X X _ X _ X],
+      ~w[X _ X _ _ X _ X _ X],
+      ~w[X _ X X _ _ _ X _ X],
+      ~w[X _ _ _ _ X _ _ _ X],
+      ~w[X X X X X X X X X X]
+    ]
+  end
+end
